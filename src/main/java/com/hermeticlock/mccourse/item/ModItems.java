52e9e7ed1c320859fb3061b767398b4898646099
JoeRez <rezenbiz@gmail.com>
2020-12-13 15:29:04 -0800

    add shovel

--- src/main/java/com/hermeticlock/mccourse/item/ModItems.java
+++ src/main/java/com/hermeticlock/mccourse/item/ModItems.java
@@ -2,8 +2,12 @@ package com.hermeticlock.mccourse.item;
 
 import com.hermeticlock.mccourse.McCourseMod;
 import com.hermeticlock.mccourse.util.Registration;
+import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 
 public class ModItems {
@@ -21,6 +25,71 @@ public class ModItems {
 
 
 
+    /* TOOLS */
+
+    public static final RegistryObject<Item> COPPER_SHOVEL =
+            Registration.ITEMS.register("copper_shovel",
+                    () -> new ShovelItem(
+                            ModItemTier.COPPER, 0f, 0f,
+                            new Item.Properties()
+                                .defaultMaxDamage(150)
+                                .addToolType(ToolType.SHOVEL, 2)
+                                .group(McCourseMod.COURSE_TAB)
+                    )
+            );
+
+
     public static void register() { }
+
+    public enum ModItemTier implements IItemTier {
+        COPPER(2, 150, 2.5f, 0f, 15,
+                Ingredient.fromStacks(new ItemStack(ModItems.COPPER_INGOT.get())));
+
+        private final int harvestLevel;
+        private final int maxUses;
+        private final float efficiency;
+        private final float attackDamage;
+        private final int enchantability;
+        private final Ingredient repairMaterial;
+
+        ModItemTier(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantability, Ingredient repairMaterial) {
+            this.harvestLevel = harvestLevel;
+            this.maxUses = maxUses;
+            this.efficiency = efficiency;
+            this.attackDamage = attackDamage;
+            this.enchantability = enchantability;
+            this.repairMaterial = repairMaterial;
+        }
+
+        @Override
+        public int getMaxUses() {
+            return maxUses;
+        }
+
+        @Override
+        public float getEfficiency() {
+            return efficiency;
+        }
+
+        @Override
+        public float getAttackDamage() {
+            return attackDamage;
+        }
+
+        @Override
+        public int getHarvestLevel() {
+            return harvestLevel;
+        }
+
+        @Override
+        public int getEnchantability() {
+            return enchantability;
+        }
+
+        @Override
+        public Ingredient getRepairMaterial() {
+            return repairMaterial;
+        }
+    }
 
 }
