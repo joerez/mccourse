00fab653b0d25e282b7e661e3d0533db9d7737b6
JoeRez <rezenbiz@gmail.com>
2020-12-13 18:16:11 -0800

    add tooltip

--- src/main/java/com/hermeticlock/mccourse/item/CopperedApple.java
+++ src/main/java/com/hermeticlock/mccourse/item/CopperedApple.java
@@ -1,10 +1,18 @@
 package com.hermeticlock.mccourse.item;
 
 import com.hermeticlock.mccourse.McCourseMod;
+import com.hermeticlock.mccourse.util.KeyboardHelper;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.World;
+
+import java.util.List;
 
 public class CopperedApple extends Item {
 
@@ -20,6 +28,17 @@ public class CopperedApple extends Item {
         );
     }
 
+    @Override
+    public void addInformation(ItemStack stack, World world, List<ITextComponent> tooltip, ITooltipFlag flag) {
+        if (KeyboardHelper.isHoldingShift()) {
+            tooltip.add(new StringTextComponent("Turns sheep into copper ingots"));
+        } else {
+            tooltip.add(new StringTextComponent("Hold " + "\u00A7e" + "SHIFT" + "\u00A77" + " for more information!"));
+        }
+
+        super.addInformation(stack, world, tooltip, flag);
+
+    }
 
 
 }
